b107198bb4eb7bbce0055a5f350a1ec266d4acad
Max Webster-khan <maxwebsterr@live.co.uk>
2025-06-28 23:40:27 +0100

    embla updates

--- src/app/_components/horizontal-gallery/horizontal-gallery.tsx
+++ src/app/_components/horizontal-gallery/horizontal-gallery.tsx
@@ -326,7 +326,7 @@ export default function HorizontalGallery() {
                                   }`}
                                   fill
                                   className={styles.thumbImageUnified}
-                                  sizes="100px"
+                                  loading="lazy"
                                 />
                               </button>
                             ))}
