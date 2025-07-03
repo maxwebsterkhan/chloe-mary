05a085e4ce6e4dd54d201b980e5add63b02056dc
Max Webster-khan <maxwebsterr@live.co.uk>
2025-07-03 17:48:38 +0100

    Improve smoothscroll

--- src/app/_components/lenis-scroll-wrapper.tsx
+++ src/app/_components/lenis-scroll-wrapper.tsx
@@ -14,7 +14,7 @@ export default function LenisScrollWrapper({
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.4,
+      duration: 1.2,
       easing: (t) => 1 - Math.pow(1 - t, 3),
       orientation: "vertical",
       gestureOrientation: "vertical",
